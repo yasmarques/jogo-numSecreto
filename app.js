e2ac16bb5f0fbfbf654b65e6ae569334d55c45ba
yasmarques <yasmin.marquestito14@gmail.com>
2024-06-24 01:00:17 -0300

    style: new style

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-alert("Jogo do Número secreto");
-
-let qntNumeros = 100;
-let numeroSecreto = parseInt(Math.random() * + qntNumeros + 1);
-let chute;
+let qntNumeros = 10;
+let numeroSecreto = numeroAleatorio();
 let tentativa = 1;
 
-while (chute != numeroSecreto) {
-      chute = prompt(`Escolha um número entre 1 e ${qntNumeros}`);
+function exibirTexto(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
-      if (chute == numeroSecreto){
-        break;
+function numeroAleatorio() {
+    return parseInt(Math.random() * + qntNumeros + 1);
+}
+
+
+
+function verificarChute() {
+    let chute = document.querySelector("input").value;
+    if (chute == numeroSecreto) {
+        acertou();
     } else {
-        if (chute > numeroSecreto){
-            alert ("ERROOOOU! Tente um número menor.");
+        if (chute > numeroSecreto) {
+            exibirTexto("h1", "ERROOOOU!");
+            exibirTexto("p", "Tente um número menor.");
         } else {
-            alert ("Errou, é um número maior que esse.");
+            exibirTexto("h1", "Ainda não");
+            exibirTexto("p", "Tente um número maior.");
         }
-
         tentativa++;
     }
 }
+function acertou() {
+    window.location.assign("acertou.html")
+    let palavraTentativa = tentativa > 1 ? "tentativas" : "tentativa";
+    exibirTexto("h2", `Você descobriu! O número secreto ${numeroSecreto} em ${tentativa} ${palavraTentativa}.`);
+}
 
-let palavraTentativa = tentativa > 1 ? "tentativas" : "tentativa";
-alert(`Você descobriu! O número secreto ${numeroSecreto} em ${tentativa} ${palavraTentativa}.`);
+function reiniciarJogo() {
 
-
-
+}
